16952460220bb90d939407ff76167e7124eaa9d9
Supratim011 <72911191+Supratim123923@users.noreply.github.com>
2022-09-07 23:17:13 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const fs = require('fs');
+//const { homedir } = require('os');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname,'../public')))
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+// app.get('/contactus',(req,res)=>{
+//   res.sendFile(contactus.html);
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
+
+
+
+
+
